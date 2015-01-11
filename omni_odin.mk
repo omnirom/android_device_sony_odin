1726fa8b8b6672dc6f8747a46e02e1b80b530629
Humberto Borba <humberos@gmail.com>
2015-01-11 18:31:18 -0200

    odin: fix typo
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ic54789b15dc94f9f547a0156fbdb59ae5f54704d

--- omni_odin.mk
+++ omni_odin.mk
@@ -1,7 +1,3 @@
-# Torch
-PRODUCT_PACKAGES := \
-    OmniTorch
-
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
@@ -10,6 +6,10 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/sony/odin/full_odin.mk)
+
+# Torch
+PRODUCT_PACKAGES += \
+    OmniTorch
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503/C6503:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6503-user 4.4.4 10.5.1.A.0.283 5P53rQ test-keys"
 
