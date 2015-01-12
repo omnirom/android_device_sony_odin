38b261bdc1502fe3cab4bdd22a8e6bf2f6799aa6
Humberto Borba <humberos@gmail.com>
2015-01-12 01:11:30 -0200

    odin: twrp: add secundary brightness path
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Id09991daf12e962849b282bb64195c9a747ef3fa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,4 +48,5 @@ TARGET_DISPLAY_GAMMA_DISABLED := true
 
 # TWRP
 DEVICE_RESOLUTION=1080x1920
-TW_BRIGHTNESS_PATH := /sys/devices/i2c-0/0-0036/leds/lm3533-lcd-bl-2/brightness
+TW_BRIGHTNESS_PATH := /sys/devices/i2c-0/0-0036/leds/lm3533-lcd-bl-1/brightness
+TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/i2c-0/0-0036/leds/lm3533-lcd-bl-2/brightness
