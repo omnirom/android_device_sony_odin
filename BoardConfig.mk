XperiaMultiROM/android_device_sony_yuga

yuga: Use a prebuilt kernel
Kernel source: https://github.com/omnirom/android_kernel_sony_apq8064/tree/12153cf382

BoardConfig.mk
@@ -25,7 +25,8 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/yuga/include
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/yuga/bluetooth
 
-TARGET_KERNEL_CONFIG := omni_fusion3_yuga_defconfig
+#TARGET_KERNEL_CONFIG := omni_fusion3_yuga_defconfig
+LOCAL_KERNEL := device/sony/yuga/prebuilt/kernel
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26

omnirom/android_device_sony_odin

Change-Id: If11fbccbd835c10017136b4cc5084b0081eb4e7d

BoardConfig.mk
@@ -25,7 +25,8 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/odin/include
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/odin/bluetooth
 
-TARGET_KERNEL_CONFIG := omni_fusion3_odin_defconfig
+#TARGET_KERNEL_CONFIG := omni_fusion3_odin_defconfig
+LOCAL_KERNEL := device/sony/odin/prebuilt/kernel
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26