7ec7a69ae3e2d562073c19a0035374afcc43051f
Humberto Borba <humberos@gmail.com>
2015-06-25 20:40:38 -0300

    odin: Update fingerprint from 10.6.A.0.454
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I58b2367e4e265ee2324485f3a62a45dfdc447ba1

--- omni_odin.mk
+++ omni_odin.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit device configuration
 $(call inherit-product, device/sony/odin/full_odin.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503/C6503:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6503-user 4.4.4 10.5.1.A.0.283 5P53rQ test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503/C6503:5.0.2/10.6.A.0.454/3395948310:user/release-keys PRIVATE_BUILD_DESC="C6503-user 5.0.2 10.6.A.0.454 3395948310 release-keys"
 
 PRODUCT_NAME := omni_odin
 PRODUCT_DEVICE := odin
